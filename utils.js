a7b9f07fd1b32cdd83b6c11d6c5ee8703c7d5b2f
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2024-07-12 15:47:22 +0500

    logs folder error handling

--- utils.js
+++ utils.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 function getLogFilePath() {
     const date = new Date();
+    if (!fs.existsSync("logs")) {
+        fs.mkdirSync("logs");
+    }
     const filename = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}.txt`;
     const logFilePath = path.join('./logs', filename)
     const fileExists = fs.existsSync(logFilePath);
