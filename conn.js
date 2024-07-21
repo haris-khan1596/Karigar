a0ca2f8fb4c22a1fe3821db7e82f2da6ad90eac6
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2024-07-21 19:16:50 +0500

    setup firestore connection

--- conn.js
+++ conn.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
+const admin = require("firebase-admin");
+const serviceAccount = require("./Firebase.json");
 
 async function connectMongodb(url) {
     return mongoose.connect(url);
 }
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 
-module.exports = {connectMongodb};
+const firestore = admin.firestore();
+
+
+module.exports = {connectMongodb, firestore};
