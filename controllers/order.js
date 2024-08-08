0fb5bf49cbcaa05e63f0bcb45de028d8842db825
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2024-08-09 00:41:49 +0500

    Order Api Updated

--- controllers/order.js
+++ controllers/order.js
@@ -142,13 +142,14 @@ async function feedbackOrder(req,res) {
             message: error.message,
         });
     }
-    const order = await Order.findByIdAndUpdate(req.params.id, {
-        $set: {
-            rating: req.body.rating,
-            feedback: req.body.feedback
-        },
-    });
-    await firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"rating": req.body.rating,"feedback": req.body.feedback});
+    const order = await Order.findById(req.params.id);
+    if(order.status !== "COMPLETED"){
+        return res.status(400).json({ message: "Order must be completed!" });
+    }
+    order.rating = req.body.rating;
+    order.feedback = req.body.feedback;
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"rating": req.body.rating,"feedback": req.body.feedback}),
+    firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"rating": req.body.rating,"feedback": req.body.feedback})]);
     return res.status(200).json({ message: "Order feedback successfully!" });
 }
 async function cancelOrder(req,res) {
@@ -163,13 +164,20 @@ async function cancelOrder(req,res) {
             message: error.message,
         });
     }
-    const order = await Order.findByIdAndUpdate(req.params.id, {
-        $set: {
-            status: "CANCELLED",
-            reason: req.body.reason
-        },
-    });
-    await firestore.collection("User").doc(`${req.user.role==="CUSTOMER"?order.worker:order.customer}`).collection("orders").doc(`${order._id}`).update({"status": "CANCELLED","reason": req.body.reason});
+    const order = await Order.findById(req.params.id);
+    if(order.status === "STARTED"){
+        return res.status(400).json({ message: "Order already started!" });
+    }
+    if(order.status === "COMPLETED"){
+        return res.status(400).json({ message: "Order already completed!" });
+    }
+    if(order.status === "CANCELLED"){
+        return res.status(400).json({ message: "Order already cancelled!" });
+    }
+    order.status = "CANCELLED";
+    order.reason = req.body.reason;
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"status": "CANCELLED","reason": req.body.reason}),
+    firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"status": "CANCELLED","reason": req.body.reason})]);
     return res.status(200).json({ message: "Order cancelled successfully!" });
 }
 
@@ -185,55 +193,67 @@ async function completeOrder(req,res) {
             message: error.message,
         });
     }
-    const order = await Order.findByIdAndUpdate(req.params.id, {
-        $set: {
-            status: "COMPLETED",
-            price: req.body.price
-        },
-    });
-    await firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"status": "COMPLETED","price": req.body.price});
+    const order = await Order.findById(req.params.id);
+    if(order.status === "COMPLETED"){
+        return res.status(400).json({ message: "Order already completed!" });
+    }
+    if(order.status === "CANCELLED"){
+        return res.status(400).json({ message: "Order already cancelled!" });
+    }
+    if(order.status === "PENDING"){
+        return res.status(400).json({ message: "Order must be Started!" });
+    }
+    order.status = "COMPLETED";
+    order.price = req.body.price;
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"status": "COMPLETED","price": req.body.price}),firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"status": "COMPLETED","price": req.body.price})]);
     return res.status(200).json({ message: "Order completed successfully!" });
 }
 
 async function startedOrder(req,res) {
-    const order = await Order.findByIdAndUpdate(req.params.id, {
-        $set: {
-            status: "STARTED",
-        },
-    });
-    await firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"status": "STARTED"});
+    const order = await Order.findById(req.params.id);
+    if(order.status === "STARTED"){
+        return res.status(400).json({ message: "Order already started!" });
+    }
+    if(order.status === "COMPLETED"){
+        return res.status(400).json({ message: "Order already completed!" });
+    }
+    if(order.status === "CANCELLED"){
+        return res.status(400).json({ message: "Order already cancelled!" });
+    }
+    order.status = "STARTED";
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"status": "STARTED"}),firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"status": "STARTED"})]);
     return res.status(200).json({ message: "Order started successfully!" });
     }
 
 async function sentPayment(req,res){
-    const order = await Order.findByIdAndUpdate(req.params.id,
-        {
-            $set: {
-                payment_status: "SENT"
-            }
-        });
-    await firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"payment_status": "SENT"});
+    const order = await Order.findById(req.params.id);
+    if(order.payment_status === "PAID"){
+        return res.status(400).json({ message: "Payment already sent!" });
+    }
+    order.payment_status = "SENT";
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"payment_status": "SENT"}),firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"payment_status": "SENT"})]);
     return res.status(200).json({ message: "Payment sent successfully!" });
 }
 
 async function paidPayment(req,res){
-    const order = await Order.findByIdAndUpdate(req.params.id,
-        {
-            $set: {
-                payment_status: "PAID"
-            }
-        });
-    await firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"payment_status": "PAID"});
+    const order = await Order.findById(req.params.id);
+    if(order.payment_status === "PAID"){
+        return res.status(400).json({ message: "Payment already paid!" });
+    }
+    order.payment_status = "PAID";
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"payment_status": "PAID"}),firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"payment_status": "PAID"})]);
     return res.status(200).json({ message: "Payment paid successfully!" });
 }
 async function unpaidPayment(req,res){
-    const order = await Order.findByIdAndUpdate(req.params.id,
-        {
-            $set: {
-                payment_status: "UNPAID"
-            }
-        });
-    await firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"payment_status": "UNPAID"});
+    const order = await Order.findById(req.params.id);
+    if(order.payment_status === "UNPAID"){
+        return res.status(400).json({ message: "Payment already unpaid!" });
+    }
+    if(order.payment_status === "PAID"){
+        return res.status(400).json({ message: "Payment already paid!" });
+    }
+    order.payment_status = "UNPAID";
+    await Promise.all([order.save(),firestore.collection("User").doc(`${order.customer}`).collection("orders").doc(`${order._id}`).update({"payment_status": "UNPAID"}),firestore.collection("User").doc(`${order.worker}`).collection("orders").doc(`${order._id}`).update({"payment_status": "UNPAID"})]);
     return res.status(200).json({ message: "Payment unpaid successfully!" });
 }
 
