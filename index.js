657b033047c0d6bba95bf56a9a8423e8e78034d6
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2024-09-28 22:16:04 +0500

    update index.js

--- index.js
+++ index.js
@@ -36,6 +36,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
     log('info', `Server is running on port ${port}`);
 });
