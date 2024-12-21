9ee6398945edb58d36be7447396b6e1043adc3bb
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2024-12-21 18:32:09 +0500

    update fileService.js

--- services/fileService.js
+++ services/fileService.js
@@ -1,12 +1,12 @@
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 
-// Initialize the Wasabi S3 
+// Initialize the Wasabi S3
 const s3 = new AWS.S3({
-  endpoint: new AWS.Endpoint('https://s3.wasabisys.com'), 
-  accessKeyId: process.env.WASABI_ACCESS_KEY,          
-  secretAccessKey: process.env.WASABI_SECRET_KEY,      
-  region: 'ap-southeast-1'                              
-});
+  endpoint: new AWS.Endpoint('https://s3.wasabisys.com'),
+  accessKeyId: process.env.WASABI_ACCESS_KEY,
+  secretAccessKey: process.env.WASABI_SECRET_KEY,
+  region: 'ap-southeast-1'
+})
 
 /**
  * Generates a pre-signed URL for uploading files to Wasabi.
@@ -14,33 +14,33 @@ const s3 = new AWS.S3({
  * @param {string} mimeType - The file's MIME type.
  * @returns {Promise<string>} - A promise that resolves to the pre-signed URL.
  */
-const createUploadPresignedUrl = async (filePath, mimeType) => {
+const createUploadPresignedUrl = async (filePath, mimeType, expires = 5) => {
   const params = {
-    Bucket: process.env.WASABI_BUCKET_NAME,   
-    Key: filePath,                            
-    Expires: 60 * 5,                          
-    ContentType: mimeType                     // Expected content type for the file
-  };
+    Bucket: process.env.WASABI_BUCKET_NAME,
+    Key: filePath,
+    Expires: expires * 60,
+    ContentType: mimeType 
+  }
 
-  return await s3.getSignedUrlPromise('putObject', params);
-};
+  return await s3.getSignedUrlPromise('putObject', params)
+}
 
 /**
  * Generates a pre-signed URL for downloading files from Wasabi.
  * @param {string} filePath - The file's path within the bucket.
  * @returns {Promise<string>} - A promise that resolves to the pre-signed URL.
  */
-const createDownloadPresignedUrl = async (filePath) => {
+const createDownloadPresignedUrl = async (filePath, expires = 5) => {
   const params = {
-    Bucket: process.env.WASABI_BUCKET_NAME,   
-    Key: filePath,                          
-    Expires: 60 * 5                           // URL expiration time in seconds (5 minutes)
-  };
+    Bucket: process.env.WASABI_BUCKET_NAME,
+    Key: filePath,
+    Expires: expires * 60
+  }
 
-  return await s3.getSignedUrlPromise('getObject', params);
-};
+  return await s3.getSignedUrlPromise('getObject', params)
+}
 
 module.exports = {
   createUploadPresignedUrl,
   createDownloadPresignedUrl
-};
+}
