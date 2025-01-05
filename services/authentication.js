c89a7872ce1d32457a6a1ce563ab140e8ea043a4
Muhammad Haris Azaz Khan <77955039+haris-khan1596@users.noreply.github.com>
2025-01-05 12:24:44 +0500

    Added Filestorage feature on Wasabi  (#6)
    * feat(file-storage): added file storage in wasabi
    
    * chore: delete fileController.js and fileRoutes.js
    
    * update fileService.js
    
    * update authentication.js
    
    ---------
    
    Co-authored-by: mahnoor-zamir <m.zamirrsh@gmail.com>

--- services/authentication.js
+++ services/authentication.js
@@ -6,7 +6,7 @@ function createTokenForUser(user) {
   const payload = {
     _id: user._id,
     name: user.name,
-    profile: user.profile||"https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg",
+    profile: user.profile,
     email: user.email,
     role: user.role,
   };
