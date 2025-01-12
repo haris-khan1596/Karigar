f6bc43bf4ce8c9a3aae74103a1cbe41c8bb211dd
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2025-01-12 14:28:29 +0500

    feat: create WorkerDetails.js

---
+++ models/WorkerDetails.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const WorkerDetailsSchema = new mongoose.Schema({
+    worker: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    worker_type: {
+        type: Array,
+        required: true
+    },
+    experience: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    
+      })
+
+module.exports = mongoose.model('WorkerDetails', WorkerDetailsSchema)
